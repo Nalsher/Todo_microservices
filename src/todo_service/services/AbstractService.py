37805b5453bc96c21354bcb1a67bd6d867bee59f
nalsher <nalsher@mail.ru>
2024-11-30 18:31:37 +0700

    feat: added some funcs in abstractService

--- src/todo_service/services/AbstractService.py
+++ src/todo_service/services/AbstractService.py
@@ -1,8 +1,22 @@
 from abc import ABC, abstractmethod
+from src.todo_service.database.repositories.AbstractRepositories import RepositoryInterface
+from fastapi import Response
 
 
 class AbstractService(ABC):
 
     @abstractmethod
-    def __init__(self, repository):
+    def __init__(self, repository: RepositoryInterface):
         self.repository = repository
+
+    @abstractmethod
+    def create(self, *args) -> Response:
+        self.repository.create(*args)
+
+    @abstractmethod
+    def delete(self, id: int) -> Response:
+        self.repository.delete(id)
+
+    @abstractmethod
+    def get(self, *args) -> Response:
+        self.repository.get(*args)
